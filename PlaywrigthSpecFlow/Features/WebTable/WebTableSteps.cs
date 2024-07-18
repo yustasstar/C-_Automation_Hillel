30986f93afa869830b3503d27dd4f729950c9543
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-07-18 12:42:50 +0300

    Specflow PW - HW finished

--- PlaywrigthSpecFlow/Features/WebTable/WebTableSteps.cs
+++ PlaywrigthSpecFlow/Features/WebTable/WebTableSteps.cs
@@ -22,6 +22,7 @@ namespace PlaywrigthSpecFlow.Features.WebTable
         [When(@"I see the WebTable")]
         public async Task WhenISeeTheWebTable() => await _WebTablesPage.IsTableVisible();
 
+
         [When(@"I see the Headers")]
         public async Task WhenISeeTheHeaders()
         {
@@ -30,7 +31,6 @@ namespace PlaywrigthSpecFlow.Features.WebTable
 
             foreach (var headerName in headersList)
             {
-                await _WebTablesPage.GoToTestPageURL();
                 await _WebTablesPage.IsTableVisible();
                 await _WebTablesPage.VerifyTableHeadersContent(headerName);
             }
@@ -39,7 +39,6 @@ namespace PlaywrigthSpecFlow.Features.WebTable
         [When(@"I type ""([^""]*)"" in the Search")]
         public async Task WhenITypeEmailInTheSearch(string email)
         {
-            await _WebTablesPage.GoToTestPageURL();
             await _WebTablesPage.FillSearchValue(email);
             await _WebTablesPage.VerifyFirstRowContentIsPresent(email);
         }
@@ -48,34 +47,55 @@ namespace PlaywrigthSpecFlow.Features.WebTable
         public async Task ThenISeeDataInTheRow(string firstName)
         {
             string headerName = "First Name";
-            await _WebTablesPage.GoToTestPageURL();
             await _WebTablesPage.IsTableVisible();
             await _WebTablesPage.IsTableRowVisible();
             await _WebTablesPage.VerifyTableContent(headerName, firstName);
         }
 
-
-
         [When(@"I click Add Button")]
-        public async Task WhenIKlickAddButton() => await _WebTablesPage.AddButtonClick();
+        public async Task WhenIClickAddButton() => await _WebTablesPage.AddButtonClick();
 
-        [When(@"I set FirstName to ""(.*)""")]
-        public async Task WhenISetFirstName(string firstName) => await _WebTablesPage.FillFirstName(firstName);
+        [When(@"I see Registration Form")]
+        public async Task WhenISeeRegistrationForm() => await _WebTablesPage.VerifyAddPopupOpened();
 
-        [When(@"I set LastName to ""(.*)""")]
-        public async Task WhenISetLastName(string lastName) => await _WebTablesPage.FillLastName(lastName);
+        [When(@"I set FirstName to ""([^""]*)""")]
+        public async Task WhenISetFirstNameTo(string firstName) => await _WebTablesPage.FillFirstName(firstName);
 
-        [Then(@"I see FirstName ""(.*)"" in a table")]
-        public async Task ThenISeeFirstName(string firstName) => await Assertions.Expect(page.GetByRole(AriaRole.Gridcell, new() { Name = firstName, Exact = true })).ToBeVisibleAsync();
+        [When(@"I set LastName to ""([^""]*)""")]
+        public async Task WhenISetLastNameTo(string lastName) => await _WebTablesPage.FillLastName(lastName);
 
-        [When(@"I set Email ""(.*)"" in a table")]
-        public async Task ThenISetEmail(string email)
+        [When(@"I set Email to ""([^""]*)""")]
+        public async Task WhenISetEmailTo(string email) => await _WebTablesPage.FillEmail(email);
+
+        [When(@"I set Age to ""([^""]*)""")]
+        public async void WhenISetAgeTo(string age) => await _WebTablesPage.FillAge(age);
+
+        [When(@"I set Salary to ""([^""]*)""")]
+        public async Task WhenISetSalaryTo(string salary)
         {
-            await page.GetByPlaceholder("name@example.com").FillAsync(email);
-            await page.GetByPlaceholder("name@example.com").PressAsync("Enter");
+            await Task.Delay(100); // Wait for 0.1 seconds
+            await _WebTablesPage.FillSalary(salary);
         }
 
-        [Then(@"I see LastName ""(.*)"" in a table")]
-        public async Task ThenISeeLastName(string lastName) => await Assertions.Expect(page.GetByRole(AriaRole.Gridcell, new() { Name = lastName, Exact = true })).ToBeVisibleAsync();
+        [When(@"I set Department to ""([^""]*)""")]
+        public async Task WhenISetDepartmentTo(string department) => await _WebTablesPage.FillDepartment(department);
+
+        [When(@"I click Submit Button")]
+        public async Task WhenIClickSubmitButton() => await _WebTablesPage.SubmitButtonCLick();
+
+        [Then(@"I see data in the row ""([^""]*)"" , ""([^""]*)"", ""([^""]*)"", ""([^""]*)"", ""([^""]*)"", ""([^""]*)""")]
+        public async Task ThenISeeDataInTheRow(string firstName, string lastName, string age, string email, string salary, string department)
+        {
+            List<string> checkList = new List<string> { firstName, lastName, age, email, salary, department };
+
+            var row = page.Locator(".rt-tr-group").First;
+            var cells = await row.Locator(".rt-td").AllInnerTextsAsync();
+            var cellList = cells.ToList();
+
+            foreach (var checkValue in checkList)
+            {
+                Assert.That(cellList, Does.Contain(checkValue), $"The search value '{checkValue}' was not found in the table.");
+            }
+        }
     }
 }
